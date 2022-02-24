ac93d63fb423865f55c40cfcb4e4d67b23eaa066
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-24 16:05:28 -0600

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+
+    function handleFocus(event) {
+        console.log("Good!")
+    }
+    function handleBlur(event) {
+        console.log("Hey! Eyes on me!")
+    }
+
+    return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+
+}
+
+export default EyesOnMe;
